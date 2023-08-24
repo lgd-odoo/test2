4da44acc6d4da4f2b7c33645059eccaaec4c5466
“Your <lgd@odoo.com>
2023-08-24 16:48:29 +0200

    ok ok 4

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class Estate(models.Model):
-   _name = "estate"
-   _description = "Estate"
-
-   name = fields.char()
+    _name = "estate"
+    _description = "Estate"
+    
+    name = fields.Char()
